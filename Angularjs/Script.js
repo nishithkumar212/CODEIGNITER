9753dd45e9071c825dd102103d0a2989a21bf837
nishithkumar212 <acha.nishith212@gmail.com>
2019-02-27 10:29:45 +0530

    27feb

--- Angularjs/Script.js
+++ Angularjs/Script.js
@@ -1,30 +1,30 @@
-var angularFormApp= angular.module('angularFormApp',[]);
-angularFormApp.controller('regController',['$scope','$http',
-function ($scope,$http)
-{
-     $scope.user = {};
-$scope.submitForm=function()
-{
-    $scope.message={};
-       $http({
-        method:'post',
-       dataType:'json',
-        data : $scope.user, //forms user object
-        url:'http://localhost/codeigniter/Welcome/add',
-        headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
-    }).then(function (data)
-    {
-        debugger;
-        if(data.data.error != '')
-   {
-   
-    $scope.alertMessage = data.data.error;
-   }
-   else
-   {  
-    window.location.href='http://localhost/codeigniter/Welcome/direction.html';
-    $scope.alertMessage = data.data.message;
-   }
-    });
-};
-}]);
+//Creating an module with the name
+var angularFormApp = angular.module('angularFormApp', []);
+
+//creating an controller with the name linked with the module
+angularFormApp.controller('regController', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.user = {};
+
+        //Creating a function 
+        $scope.submitForm = function () {
+            $scope.message = {};
+            $http({
+                method: 'post',
+                dataType: 'json',
+                data: $scope.user, //forms user object
+                url: 'http://localhost/codeigniter/Welcome/add',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).then(function (data) {
+                debugger;
+                if (data.data.error != '') {
+                    //Assigning the error to the message
+                    $scope.alertMessage = data.data.error;
+                }
+                else {
+                    //Asssigning the message 
+                    $scope.alertMessage = data.data.message;
+                }
+            });
+        };
+    }]);
--- Angularjs/Scripts.js
+++ Angularjs/Scripts.js
@@ -1,30 +1,33 @@
-angular.module('Myform',[])
-.controller('appcontroller',['$scope','$http',
-function($scope,$http)
-{
-    $scope.user = {};
-$scope.submitForm=function()
-{
-    $scope.message={};
-       $http({
-        method:'post',
-       dataType:'json',
-        data : $scope.user, //forms user object
-        url:'http://localhost/codeigniter/Welcome/login',
-        headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
-    }).then(function (data)
-    {
-        debugger
-        if(data.data.error != '')
-        {
-         $scope.alertClass = 'alert-danger';
-         $scope.alertMessage = data.data.error;
-        }
-        else
-        {  
-         $scope.alertClass = 'alert-success';
-         $scope.alertMessage = data.data.message;
-        }
-    });
-};
-}]);
+//creating a module
+angular.module('Myform', [])
+
+    //creating the controller
+    .controller('appcontroller', ['$scope', '$http',
+        function ($scope, $http) {
+            $scope.user = {};
+
+            //Creating a function 
+            $scope.submitForm = function () {
+                $scope.message = {};
+                $http({
+                    method: 'post',
+                    dataType: 'json',
+                    data: $scope.user, //forms user object
+                    //calling the api
+                    url: 'http://localhost/codeigniter/Welcome/login',
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                }).then(function (data) {
+                    debugger
+                    if (data.data.error != '') {
+                        $scope.alertClass = 'alert-danger';
+                        //Assigning the error to the message
+                        $scope.alertMessage = data.data.error;
+                    }
+                    else {
+                        $scope.alertClass = 'alert-success';
+                        //Assigning the error to the message
+                        $scope.alertMessage = data.data.message;
+                    }
+                });
+            };
+        }]);
