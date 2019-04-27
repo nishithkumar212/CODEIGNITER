d39743f00d2239218655a4ed2460c238250c58bf
nishithkumar212 <acha.nishith212@gmail.com>
2019-04-27 11:12:19 +0530

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');  
+const path=require('path');
 const app = express();  
-app.use(express.static(__dirname + '/dist'));  
+app.use(express.static(__dirname + '/dist/Fundoo'));  
 app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+  res.status(200).sendFile(__dirname + '/dist/Fundoo/index.html');  
 });  
 app.listen(process.env.PORT || 8080);  
